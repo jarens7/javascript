26e3e49de492d4d8ebe212f367e5009e5511fc50
josh_arens <josh.arens@beanworks.com>
2018-10-04 21:58:34 -0700

    more goodies

---
+++ countRowsCountColumnsGrid.js
@@ -0,0 +1,21 @@
+let countRows = function() {
+    sum = 0
+    for (let i = 0; i < GRID.length; i++) {
+        sum ++
+    }
+    return sum
+}
+
+function countColumns() {
+    let sum = 0;
+  GRID.forEach(function(grid) {
+      sum = grid.length;
+  }) 
+  return sum
+}
+
+function gridSize() {
+    let rowSize = countRows() 
+    let columnSize = countColumns() 
+    return columnSize + ' x ' + rowSize
+}
